6fe60b7b7acbeb55675aca8d8cdd487dc8e33d49
Marci McClenon <mccmarci@gmail.com>
2019-03-28 12:14:16 -0500

    cleanup

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.post('/', (req, res) => {console.log(req.body); res.json(req.body)});
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
