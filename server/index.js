1a95f4b07d279e10fd3319e2a3c6b52992438d7b
Marci McClenon <mccmarci@gmail.com>
2019-03-28 12:11:51 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const port = 3001;
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.post('/', (req, res) => {console.log(req.body); res.json(req.body)});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
